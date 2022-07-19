26a79cfdbf01107caa667322ba8466cef4bb044a
Eli <+>
2022-07-18 21:17:36 -0400

    commit tree 00fa68b945021b0894a7e7be58d6a6b09829f78f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork(activity);
+
+function saturdayFun(activity = "roller-skate") {
+
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function wrapAdjective(asterick) {
+    return function (param = "special") {
+        return `You are ${asterick}${param}${asterick}!`;
+    }
+}
